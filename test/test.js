1c13db34ba8962d429d6311b56b7038dbca89db2
JD Isaacks <jd@jisaacks.com>
2014-12-27 01:03:25 -0500

    Use supertest instead of request for testing api endpoints

--- test/test.js
+++ test/test.js
@@ -2,10 +2,9 @@ var express     = require("express");
 var Datastore   = require('nedb');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
+var request     = require('supertest');
 var app         = express();
 var json_api    = require("../index");
-var request     = require('request');
-var httpUtils   = require('request-mocha')(request);
 
 var server;
 var resource_id = "abcdefg";
@@ -15,126 +14,82 @@ app.use(bodyParser.json());
 
 app.use("/data/resources", json_api(store));
 
-app.get("/", function(req, res) {
-  res.send("Server Running");
-});
+describe('JSON API', function () {
+  describe('POST data to mounted URL', function () {
+    it('creates a record', function (done) {
+      request(app)
+        .post("/data/resources")
+        .set("Accept", "application/json")
+        .send({foo: "bar", _id: resource_id})
+        .end(function(err, res){
+          expect(res.statusCode).to.equal(200);
+          expect(res.body.foo).to.equal("bar");
+          done(err);
+        });
+    });
+  });
 
-var startServer = function(){
-  server = app.listen(8025);
-};
-var stopServer = function(){
-  server.close();
-};
+  describe('GET mounted URL', function () {
+    it('returns records', function (done) {
+      request(app)
+        .get("/data/resources")
+        .set("Accept", "application/json")
+        .end(function(err, res){
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an("array");
+          done(err);
+        });
+    });
+  });
 
-describe('Test Server', function () {
-  before(startServer);
-  after(stopServer);
+  describe('GET /:id', function () {
+    it('returns a single record with matching id', function (done) {
+      request(app)
+        .get("/data/resources/" + resource_id)
+        .set("Accept", "application/json")
+        .end(function(err, res){
+          expect(res.statusCode).to.equal(200);
+          expect(res.body._id).to.be.equal(resource_id);
+          done(err);
+        });
+    });
 
-  httpUtils.save('http://localhost:8025/');
-  
-  it('should be running', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(200);
-    expect(this.body).to.equal('Server Running');
+    it('repsonds with 404 for missing record', function (done) {
+      request(app)
+        .get("/data/resources/missing")
+        .set("Accept", "application/json")
+        .end(function(err, res){
+          expect(res.statusCode).to.equal(404);
+          done(err);
+        });
+    });
+  });
+
+  describe('PUT data to /:id', function () {
+    it('updates the record with matching id', function (done) {
+      request(app)
+        .put("/data/resources/" + resource_id)
+        .set("Accept", "application/json")
+        .send({foo: "bam"})
+        .end(function(err, res){
+          expect(res.statusCode).to.equal(200);
+          expect(res.body._id).to.equal(resource_id);
+          expect(res.body.foo).to.equal("bam");
+          done(err);
+        });
+    });
+  });
+
+  describe('DELETE /:id', function () {
+    it('deletes the record with matching id', function (done) {
+      request(app)
+        .delete("/data/resources/" + resource_id)
+        .set("Accept", "application/json")
+        .end(function(err, res){
+          expect(res.statusCode).to.equal(200);
+          done(err);
+        });
+    });
   });
 });
 
-describe('POST data to mounted URL', function () {
-  before(startServer);
-  after(stopServer);
-
-  httpUtils.save({
-    method: 'POST',
-    url: 'http://localhost:8025/data/resources',
-    json: {
-      foo: 'bar',
-      _id: resource_id
-    }
-  });
-  
-  it('creates a record', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(200);
-    expect(this.body.foo).to.equal("bar");
-  });
-});
-
-describe('GET mounted URL', function () {
-  before(startServer);
-  after(stopServer);
-
-  httpUtils.save({
-    method: 'GET',
-    url: 'http://localhost:8025/data/resources'
-  });
-  
-  it('returns records', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(200);
-    expect(JSON.parse(this.body)).to.be.an('array');
-  });
-});
-
-describe('GET /:id', function () {
-  before(startServer);
-  after(stopServer);
-
-  httpUtils.save({
-    method: 'GET',
-    url: 'http://localhost:8025/data/resources/' + resource_id
-  });
-  
-  it('returns a single record with matching id', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(200);
-    expect(JSON.parse(this.body)._id).to.equal(resource_id);
-  });
-});
-
-describe('PUT data to /:id', function () {
-  before(startServer);
-  after(stopServer);
-
-  httpUtils.save({
-    method: 'PUT',
-    url: 'http://localhost:8025/data/resources/' + resource_id,
-    json: {foo: "bam"}
-  });
-  
-  it('updates the record with matching id', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(200);
-    expect(this.body._id).to.equal(resource_id);
-    expect(this.body.foo).to.equal("bam");
-  });
-});
-
-describe('DELETE /:id', function () {
-  before(startServer);
-  after(stopServer);
-
-  httpUtils.save({
-    method: 'DELETE',
-    url: 'http://localhost:8025/data/resources/' + resource_id
-  });
-  
-  it('deletes the record with matching id', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(200);
-  });
-});
-
-describe('GET /:id with a bad id', function () {
-  before(startServer);
-  after(stopServer);
-
-  httpUtils.save({
-    method: 'GET',
-    url: 'http://localhost:8025/data/resources/missing'
-  });
-  
-  it('response with 404', function () {
-    expect(this.err).to.equal(null);
-    expect(this.res.statusCode).to.equal(404);
-  });
-});
